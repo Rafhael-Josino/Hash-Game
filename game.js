8257422eb7f144039ed7feb36db98dd6a642c25c
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-06-26 21:16:48 -0300

    Initial Commit

---
+++ game.js
@@ -0,0 +1,307 @@
+/*
+ * Hash Game's script
+ * 
+ * Contains all the functions to the game work
+ * 
+ * Some of this functions will be separeted in a module,
+ * so they can be used for general use
+ *
+ * The players's data is handled by the server and has the following formatting in .txt files:
+ * 		nine first chars: game's table ["x", "o", "_"]
+ *		10º char: game's turn ["x", "o"]
+ *		11º char: player's symbol ["x", "o"] exclusive
+ *
+ * The 11º char is still to be implemented and only to the mode against machine
+ */
+
+// ################################# DOM elements #######################################
+let restartButton = document.getElementById("restart");
+let deleteButton = document.getElementById("delete");
+let createButton = document.getElementById("create");
+let turn = document.getElementById("turn");
+let playerSymbol = document.getElementById("player");
+let select = document.querySelector("select");
+let menu = document.getElementById("menu");
+let game = document.getElementById("game");
+let buttons = document.getElementById("buttons");
+let title = document.querySelector("title");
+
+// Select element with options X and O to temporary use in Reset and newPlayer functions
+let newPlayerSymbol = document.createElement("select");
+let xSymbol = document.createElement("option");
+xSymbol.setAttribute("value", 'x');
+xSymbol.innerHTML = "x";
+let oSymbol = document.createElement("option");
+oSymbol.setAttribute("value", 'o');
+oSymbol.innerHTML = "o";
+newPlayerSymbol.appendChild(xSymbol);
+newPlayerSymbol.appendChild(oSymbol);
+
+
+
+// ################################## Other bindings ####################################
+let symbol, cell, match, player;
+let playerName = new RegExp('\\w+.txt', 'g');
+let newPlayer;
+
+// To be check if is necessary
+select.value = "default";
+
+
+
+// #################################### Functions #######################################
+
+// Loads a game from the server according to the current player selected
+function loadGame() {
+    // If the game has not yet started, makes the necessary alterations
+	// This condition is checked by the value of the DOM game's class
+    if (game.getAttribute("class") === "invisible") {
+        document.getElementById("default").remove();
+        restartButton.addEventListener("click", restart);
+        deleteButton.addEventListener("click", deletePlayer);
+        game.setAttribute("class", "visible");
+    }
+	// Fetches the selected player's information
+    fetch('/players/' + player).then(resp => resp.text())
+        .then(text => {
+            console.log("teste do [text] em loadGame:", text);
+            for (let i = 0; i < 9; i++) {
+                let cell = document.getElementById(String(i));
+                cell.textContent = text[i];
+                if (text[i] === '_') {
+					// !!! Must confirm if the event handlers can be add always,
+					// once their callback function check the state (class) of the space
+					// That being the case, they can very well be added at the page's loading
+                    cell.addEventListener("click", marked);
+                    cell.addEventListener("mousemove", markPreview);
+                    cell.addEventListener("mouseleave", cleanPreview);
+                    cell.setAttribute("class", "empty");
+                }
+                else
+                    cell.setAttribute("class", "full");
+            }
+            symbol = text[9];
+            turn.textContent = symbol;
+			title.innerHTML = symbol.toUpperCase() + " Hash Game";
+            console.log("Game loaded - this turn:", symbol);
+        });
+}
+
+// Shows the symbol that would be selected in an empty space of the table as the mouse move above it
+function markPreview(event) {
+    if (event.target.getAttribute("class") === 'empty') event.target.textContent = symbol;
+}
+
+// Cleans the symbol that would be selected in an empty space of the table as the mouse leaves it
+function cleanPreview(event) {
+    if (event.target.getAttribute("class") === 'empty') event.target.textContent = '_';
+}
+
+// Marks the symbol in the selected place, according to the current turn
+// The change at each time this function is called is saved in the player's file by the server
+function marked(event) {
+	if (event.target.getAttribute("class") === 'empty') {
+		    event.target.setAttribute("class", "full");
+		    fetch("mark", {
+		        method: "POST",
+		        headers: {"Content-Type": "application/json"},
+		        body: JSON.stringify({
+		            pos: event.target.getAttribute("id"),
+		            name: player
+		        })
+		    }).then(resp => resp.text()).then(text => {
+		            try {
+		                if (text !== 'e') {
+                		    console.log("next turn -", text);
+                		    symbol = text;
+		                    turn.textContent = symbol;
+							title.innerHTML = symbol.toUpperCase() + " Hash Game";
+	                	}	
+        		        else throw "file corrupted";
+		            } catch(error) {
+		                console.log(error);
+                		symbol = 'e';
+		                turn.textContent = "File corrupted, please try restart game";
+		            }		
+        		});
+	}
+}
+
+// Restart the game of the current player
+// This change is saved in the player's file by the server
+function restart() {
+	restartButton.removeEventListener("click", restart);
+	restartButton.innerHTML = "Confirm";
+	restartButton.addEventListener("click", confirmRestart);
+	deleteButton.removeEventListener("click", deletePlayer);
+	deleteButton.innerHTML = "Cancel";
+	deleteButton.addEventListener("click", cancelRestart);
+
+	// Insert option to chose symbol
+	buttons.appendChild(newPlayerSymbol);
+
+	function confirmRestart() {
+		fetch("restart", {
+			method: "POST",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify({
+				name: player,
+				symbol: newPlayerSymbol.value
+			})
+		}).then(resp => {
+			for (let i = 0; i < 9; i++) {
+				cell = document.getElementById(String(i));
+				cell.textContent = '_';
+				cell.addEventListener("click", marked);
+				cell.addEventListener("mousemove", markPreview);
+				cell.addEventListener("mouseleave", cleanPreview);
+				cell.setAttribute("class", "empty");
+			}
+			symbol = newPlayerSymbol.value;
+			turn.textContent = symbol;
+			title.innerHTML = symbol.toUpperCase() + " Hash Game";
+			console.log("Game restarted");
+			cancelRestart();
+		});
+	}
+
+	function cancelRestart() {
+		restartButton.removeEventListener("click", confirmRestart);
+		restartButton.innerHTML = "Restart";
+		restartButton.addEventListener("click", restart);
+		deleteButton.removeEventListener("click", cancelRestart);
+		deleteButton.innerHTML = "Delete";
+		deleteButton.addEventListener("click", deletePlayer);
+		buttons.removeChild(newPlayerSymbol);
+	}
+}
+
+// Gets from server the list of players saved
+function getPlayers() {
+    fetch("players").then(resp => resp.text())
+        .then(text => {
+            console.log("Players fetched:\n", text);
+            match = text.match(playerName);
+            if(match) {
+                match.forEach(name => {
+                    // name -> .txt
+                    // onlyName -> without extension part
+                    let onlyName = name.slice(0,-4); 
+                    let player = document.createElement("option");
+                    player.innerHTML = onlyName;
+                    player.setAttribute("value", name); 
+                    player.setAttribute("id", name);
+                    select.appendChild(player);
+                });
+            }
+            else {
+                console.log("There is no players so far :(");
+            }
+        });	
+}
+
+// Sends to the server a requisition to create a new player, with the name and symbol of choice
+// After created, automatically select this player
+function createPlayer() {
+    createButton.removeEventListener("click", createPlayer);
+    createButton.innerHTML = "Confirm player/symbol (put in blank to cancel)";
+    createButton.addEventListener("click", confirmNewPlayer);
+
+    let newPlayerField = document.createElement("input");
+	newPlayerField.focus(); // Not working
+    menu.appendChild(newPlayerField);
+    menu.appendChild(newPlayerSymbol);
+    
+    function confirmNewPlayer() {
+        let match = newPlayerField.value.match(new RegExp('\\w+'));
+        // If a valid name was chosen
+        if (match) {
+            console.log("New player:", match[0]);
+            fetch("create", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    name: match[0],
+                    symbol: newPlayerSymbol.value
+                })
+            }).then(resp => {
+                console.log("Status", resp.status);
+                if (resp.status == 201) {
+                    console.log(match[0], "created");
+                    player = match[0] + ".txt";
+                    let newPlayer = document.createElement("option");
+                    newPlayer.innerHTML = match[0];
+                    newPlayer.setAttribute("value", player);
+                    newPlayer.setAttribute("id", player);
+                    select.appendChild(newPlayer);
+                    select.value = player;
+                    loadGame();
+                }
+                else if (resp.status == 200) {
+                    console.log(match[0], "already present");
+                }
+                else if (resp.status == 403) {
+                    console.log("Number maximum of players reached");
+                }
+                cancelNewPlayer();
+            })
+        }
+        else {
+            console.log("No name chosen");
+            cancelNewPlayer();
+        }
+    }
+
+    function cancelNewPlayer() {
+        createButton.removeEventListener("click", confirmNewPlayer);
+        createButton.innerHTML = "Create new player";
+        createButton.addEventListener("click", createPlayer);
+        menu.removeChild(newPlayerField);
+        menu.removeChild(newPlayerSymbol);
+    }    
+}
+
+// Sends requisition to server to delete a player, this is, the server delete the player's file
+function deletePlayer() {
+    fetch('delete', {
+        method: 'DELETE',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: player
+        })
+    }).then(resp => resp.text()).then(text => {
+        console.log(text);
+        console.log(player);
+        document.getElementById(player).remove();
+        player = select.value;
+
+        // If there is players, loads the game of the one catched, that is, the first option DOM element
+        if (player) 
+			loadGame();
+
+        // If there is no more players, returns the page to its initial state, without a present game
+        else {
+            let def = document.createElement("option");
+            def.setAttribute("id", "default");
+            def.innerHTML = "-";
+            select.appendChild(def);
+            game.setAttribute("class", "invisible");
+            restartButton.removeEventListener("click", restart);
+           	deleteButton.removeEventListener("click", deletePlayer);
+			title.innerHTML = "# Hash Game";
+        }
+    });
+}
+
+// Calls loadGame function after a player is chosen
+function chosePlayer(event) {
+    player = event.target.value;
+    console.log(player, "selected");
+    loadGame();
+}
+
+// ################################## Starting Game ####################################
+createButton.addEventListener("click", createPlayer);
+getPlayers();
+select.addEventListener("change", chosePlayer);
+title.innerHTML = "# Hash Game";
---
+++ gameServer.js
@@ -0,0 +1,196 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors= require('cors');
+const fs = require('fs');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const maxPlayers = 10;
+let numberOfPlayers = 0;
+
+// For now, the first player will always be "x"
+const newGame = "_________";
+
+app.post('/restart', (req, res) => {
+	let playerSymbol = req.body.symbol;
+	fs.writeFile(path.join(__dirname, "players", req.body.name), newGame+playerSymbol, err => {
+		if (err) console.log("Restart error:", err);
+		else {
+			console.log("Game restarted");
+			res.send();
+		}
+	});
+});
+
+// Include the status in the response of the server, not only the text sent
+// Verify this interaction with game.js
+app.post('/create', (req, res) => {
+	let fileName = req.body.name + '.txt';
+	let playerSymbol = req.body.symbol;
+	fs.readdir(path.join(__dirname, "players"), (err, files) => {
+		if (err) console.log("Unable to scan directory:", err);
+		else {
+			if (numberOfPlayers === maxPlayers) {
+				console.log("Maximum number of players");
+				res.status(403).send();
+			}
+			else if (files.includes(fileName)) {
+				console.log(req.body.name, "already included");
+				res.status(200).send();
+			}
+			else {
+				fs.writeFile(path.join(__dirname, "players", fileName), newGame+playerSymbol, err => {
+					if (err) console.log("Create error:", err);
+					else {
+						numberOfPlayers++;
+						console.log(req.body.name, "created");
+						res.status(201).send();
+					}
+				})	
+			}
+		}
+	})
+});
+
+app.delete('/delete', (req, res) => {
+	let name = req.body.name;
+	console.log("Deleting player", name);
+	namePath = path.join(__dirname, "players", name);
+	fs.unlink(namePath, (err) => {
+		if (err) console.log(err);
+		else {
+			if (numberOfPlayers) numberOfPlayers--;
+			console.log(numberOfPlayers, "players");
+			res.send("Server here: Player " + name + " deleted");
+		}
+	})
+});
+
+let pos, name, newSymbol;
+app.post('/mark', (req, res) => {
+	console.log("POST /mark:", req.body);
+	pos = parseInt(req.body.pos);
+	name = req.body.name;
+	namePath = path.join(__dirname, "players", name);
+		fs.readFile(namePath, "utf8", (err, data) => {
+		if (err) console.log(err);
+		else {
+			console.log("data read:", data, "symbol:", data[9]);
+			try {
+				if (data[9] === "x") newSymbol = "o";
+				else if (data[9] === "o") newSymbol = "x";
+				else throw "file currupted";
+				data = data.slice(0, pos) + data[9] + data.slice(pos + 1, 9);
+				console.log("pre-data", data);
+				data = data + newSymbol;
+				// Must change the method of warn the client to use status
+				fs.writeFile(namePath, data, err => {
+					if (err) {
+						console.log("Write file error:", err);
+						res.send("e");
+					}
+					else {
+						res.send(newSymbol);
+						console.log("data sent:", data);
+					}
+				});
+			} catch(error) {
+				console.log(error);
+				res.send("e");
+			}
+		}
+	});
+});
+
+app.get('/players', (req, res) => {
+	console.log('List of players saved:');
+	fs.readdir(path.join(__dirname, 'players'), (err, files) => {
+		if (err) console.log("Unable to scan directory:", err);
+		else {
+			numberOfPlayers = files.length;
+			console.log(numberOfPlayers, "players:");
+			files.forEach(file => console.log(file));
+			res.send(files);
+		}
+	});
+});
+
+app.get('/players/:player', (req, res) => {
+	console.log("Loading player", req.params.player);
+	namePath = path.join(__dirname, "players", req.params.player);
+	fs.readFile(namePath, "utf8", (err, data) => {
+	if (err) console.log(err);
+		else {
+			console.log("data read:", data, "symbol:", data[9]);
+			try {
+				// Think new ways to check the files
+				if (data[9] === 'x' || data[9] === 'o') {
+					console.log("load method - data sent:", data);
+					//res.type('json');
+					res.send(data);
+				}
+				else throw "file corrupted in /load";
+			} catch(error) {
+				console.log(error);
+				console.log("Trying rewrite file, adding this player again");
+				//How to procede from here?
+				//newPlayer(req.params.player, namePath);
+				//res.type('json');
+				res.send(newGame);
+			}
+		}
+	});
+})
+
+app.get('/index.html', (req, res) => {
+	var options = {
+		root: __dirname,
+		dotfiles: 'deny',
+		headers: {
+			'x-timestam': Date.now(),
+			'x-sent': true
+		}
+	};
+
+	res.sendFile('index.html', options, (err) => {
+		if (err) console.log("Error getting index page", err);
+		else console.log('index.html sent');
+	});
+});
+
+app.get('/game.js', (req, res) => {
+	var options = {
+		root: __dirname,
+		dotfiles: 'deny',
+		headers: {
+			'x-timestam': Date.now(),
+			'x-sent': true
+		}
+	};
+
+	res.sendFile('game.js', options, (err) => {
+		if (err) console.log("Error getting index page", err);
+		else console.log('game.js sent');
+	});
+});
+
+app.get('/game.css', (req, res) => {
+	var options = {
+		root: __dirname,
+		dotfiles: 'deny',
+		headers: {
+			'x-timestam': Date.now(),
+			'x-sent': true
+		}
+	};
+
+	res.sendFile('game.css', options, (err) => {
+		if (err) console.log("Error getting index page", err);
+		else console.log('game.css sent');
+	});
+});
+
+app.listen(8000 , () => console.log("listening"));
